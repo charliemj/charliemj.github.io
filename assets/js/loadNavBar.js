62da6100723ac034873f6ff1ca988cae50d126f0
Karleigh Moore <karleighmoore19@gmail.com>
2017-05-12 21:10:08 -0400

    Navbar js

---
+++ assets/js/loadNavBar.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $("#nav").append("<a href='projects.html'>Projects</a>");
+    $("#nav").append("<a href='blog'>Blog</a>");
+    $("#nav").append("<a href='contact.html'>Contact</a>");
+    $("#nav").append("<a href='assets/kjmoore.pdf'>Resume</a>"); 
+
+});
