fce44cf5970aa3ea1b5d647da9d1ab3bb987418c
Karleigh Moore <karleighmoore19@gmail.com>
2018-09-19 15:47:50 -0400

    .

--- assets/js/loadNavBar.js
+++ assets/js/loadNavBar.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     var analytics ='<!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=UA-112423693-1"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "UA-112423693-1");</script>';
     $("head").append(analytics);
     $("#nav").append("<a href='projects.html'>Projects</a>");
-    $("#nav").append("<a href='https://charliemj.github.io/blog/'>Blog</a>");
     $("#nav").append("<a href='contact.html'>Contact</a>");
     $("#nav").append("<a href='assets/resume.pdf'>Resume</a>");
 
